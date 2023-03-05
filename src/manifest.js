736046956767e01bf83f204c43109729fb7b7bcc
Roman Zipp <ich@ich.wtf>
2023-03-05 14:20:39 +0100

    Fix permissions

--- src/manifest.js
+++ src/manifest.js
@@ -43,12 +43,7 @@ const sharedManifest = {
         open_in_tab: true,
     },
     permissions: [
-        '<all_urls>',
-        '*://*/*',
         'storage',
-        // hostMatches.youtube,
-        // hostMatches.api,
-        // hostMatches.frontend,
     ],
 };
 
