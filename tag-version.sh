f27225dc6eb0d4c78a27af3cc17c39b35ae9eb4a
Roman Zipp <ich@ich.wtf>
2023-09-13 18:30:47 +0200

    Use bun in build workflow & Replace yarn versioning with jq

--- tag-version.sh
+++ tag-version.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 VERSION=$(echo $1|sed -r 's/-dev\.[0-9]+//')
-yarn version --new-version $VERSION --no-git-tag-version
+
+echo "Version: $VERSION"
+
+jq ".version = \"$VERSION\"" package.json > package.json.tmp
+mv package.json.tmp package.json
+
+# yarn version --new-version $VERSION --no-git-tag-version
