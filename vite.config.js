dc24d3a8e6a578ad9262c121aadbc7d1d0577c0b
Roman Zipp <ich@ich.wtf>
2024-03-09 13:56:05 +0100

    Load vite react plugin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 import webExtension from '@samrum/vite-plugin-web-extension';
 import eslint from 'vite-plugin-eslint';
 import { getManifest } from './src/manifest';
@@ -10,6 +11,7 @@ export default defineConfig(({ mode }) => {
 
     return {
         plugins: [
+            react(),
             eslint(),
             webExtension({
                 manifest: getManifest(Number(env.MANIFEST_VERSION), env.BROWSER_TARGET, env),
