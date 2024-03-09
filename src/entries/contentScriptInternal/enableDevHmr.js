f453170c121f60ff94b58a47a0ec46843c4c6d99
Roman Zipp <ich@ich.wtf>
2024-03-09 14:21:27 +0100

    Move dev hmr to root

--- src/entries/contentScriptInternal/enableDevHmr.js
+++
@@ -1,8 +0,0 @@
-import RefreshRuntime from 'react-refresh';
-
-if (import.meta.hot) {
-    RefreshRuntime.injectIntoGlobalHook(window);
-    window.$RefreshReg$ = () => {};
-    window.$RefreshSig$ = () => (type) => type;
-    window.__vite_plugin_react_preamble_installed__ = true;
-}
