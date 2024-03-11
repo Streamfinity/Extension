cb5fb51b2b3d818e85c5be3f86d5e3b269851fa7
Roman Zipp <ich@ich.wtf>
2024-03-11 08:10:49 +0100

    Refactor background script message handling

---
+++ src/entries/background/common/actions.js
@@ -0,0 +1,170 @@
+import browser from 'webextension-polyfill';
+import {
+    storageGetToken, clearStorage, storageSetToken, storageSetSettingVisible,
+} from '~/entries/background/common/storage';
+import { getExtensionStatus, createPlaybackProgress, getAuthenticatedUser } from '~/entries/background/common/api';
+import { EVENT_REFRESH_AUTH, EVENT_REFRESH_SETTINGS } from '~/messages';
+import { why } from '~/common/pretty';
+import { sendMessageToContentScript } from '~/entries/background/common/spaceship';
+
+/**
+ * @type {{user}}
+ */
+let extensionStatus = {};
+
+export async function getStatus() {
+    const token = await storageGetToken();
+
+    if (!token) {
+        return { user: null };
+    }
+
+    const { data } = await getExtensionStatus();
+
+    extensionStatus = data?.data || {};
+
+    // WARNING: browser.action is not available in Firefox for some reason
+    if (browser.action) {
+        if (extensionStatus?.accounts?.length > 0) {
+            await browser.action.setBadgeText({ text: `${extensionStatus.accounts.length}` });
+            await browser.action.setBadgeTextColor({ color: '#fff' });
+            await browser.action.setBadgeBackgroundColor({ color: '#f00' });
+        } else {
+            await browser.action.setBadgeText({ text: null });
+        }
+    }
+
+    return {
+        data: data?.data,
+    };
+}
+
+export async function logout() {
+    await clearStorage();
+
+    await sendMessageToContentScript(EVENT_REFRESH_AUTH);
+
+    return { success: true };
+}
+
+export async function sendPlayerProgress(data) {
+    if (!extensionStatus.user) {
+        log.debug('no status adata, dont send player progress');
+        return;
+    }
+
+    if (extensionStatus.live_streams.length === 0) {
+        log.debug('on stream live');
+        return;
+    }
+
+    await createPlaybackProgress({
+        data,
+        liveStreamId: extensionStatus.live_streams[0].id,
+    });
+}
+
+export async function loginFetchUser(accessToken) {
+    try {
+        const { data } = await getAuthenticatedUser({
+            token: accessToken,
+        });
+
+        log.debug('login validate', 'ok');
+        log.debug('storing token...', accessToken);
+
+        await storageSetToken(accessToken);
+
+        return { user: data.data };
+    } catch (err) {
+        log.error('error checking data', err);
+    }
+
+    return { user: null };
+}
+
+export async function login() {
+    const redirectUri = browser.identity.getRedirectURL();
+
+    const url = `${import.meta.env.VITE_API_URL}/oauth/authorize?${new URLSearchParams({
+        client_id: import.meta.env.VITE_OAUTH_CLIENT_ID,
+        response_type: 'token',
+        redirect_uri: redirectUri,
+        scope: '',
+    })}`;
+
+    log.debug('login', 'redirect', redirectUri);
+    log.debug('login', url);
+
+    try {
+        // https://cfiiggfhnccbchheekifachmajflgcgn.chromiumapp.org/
+        //     #access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ
+        //     &token_type=Bearer
+        //     &expires_in=31622400
+        const responseUrl = await browser.identity.launchWebAuthFlow({
+            url,
+            interactive: true,
+        });
+
+        const parsedUrl = new URL(responseUrl);
+        const paramsString = parsedUrl.hash.substring(1);
+        const params = new URLSearchParams(paramsString);
+
+        const accessToken = params.get('access_token');
+
+        log.debug('login', 'received success response');
+
+        const response = await loginFetchUser(accessToken);
+
+        await sendMessageToContentScript(EVENT_REFRESH_AUTH, {});
+
+        return response;
+    } catch (err) {
+        log.warn('login', 'error', err);
+
+        return {
+            success: false,
+            error: why(err),
+        };
+    }
+}
+
+export async function updateSettingUpdateVisible(data) {
+    await storageSetSettingVisible(data.visible);
+
+    await sendMessageToContentScript(EVENT_REFRESH_SETTINGS);
+
+    return {};
+}
+
+export async function openPopup() {
+    // WARNING: browser.action is not available in Firefox for some reason
+    if (!browser.action || !('openPopup' in browser.action)) {
+        log.warn('openPopup', 'not available in browser.action');
+        return {};
+    }
+
+    await browser.action?.openPopup();
+
+    return {};
+}
+
+export async function updateScheme({ dark }) {
+    if (!browser.action) {
+        log.warn('updateScheme', 'no browser action available');
+        return;
+    }
+
+    const sizes = ['16', '32', '48', '128'];
+    const icons = {};
+
+    sizes.forEach((size) => {
+        icons[size] = `icons/transparent/${dark ? 'light' : 'dark'}-${size}.png`;
+    });
+
+    log.debug('updateScheme', dark, icons);
+
+    await browser.action.setIcon({
+        path: icons,
+    });
+}
--- src/entries/background/common/controllers.js
+++ src/entries/background/common/controllers.js
@@ -1,269 +1,35 @@
-import browser from 'webextension-polyfill';
-import {
-    searchSuggestionAccounts,
-    submitSuggestion,
-    getWatchedReactions,
-    submitReaction,
-    getReactionPolicy,
-    getContentRatings,
-    createPlaybackProgress,
-    getExtensionStatus, getAuthenticatedUser, getReactionsForVideo, getOriginalVideosForVideo,
-} from '~/entries/background/common/api';
-import {
-    storageGetToken, clearStorage, storageGetAll, storageSetToken, storageSetSettingVisible,
-} from '~/entries/background/common/storage';
-import {
-    GET_STATUS,
-    PLAYER_PROGRESS,
-    LOGOUT,
-    SUGGESTIONS_SEARCH_ACCOUNT, SUGGESTIONS_SUBMIT,
-    WATCHED_REACTIONS_GET,
-    REACTION_SUBMIT, REACTION_POLICY_GET,
-    LOGIN, CONTENT_RATINGS_GET, REACTIONS_GET_FOR_VIDEO,
-    SETTING_UPDATE_VISIBLE, EVENT_REFRESH_SETTINGS, OPEN_POPUP, SET_BROWSER_THEME, EVENT_REFRESH_AUTH, REACTIONS_GET_ORIGINAL_VIDEOS,
-} from '~/messages';
+import * as api from '~/entries/background/common/api';
+import * as messages from '~/messages';
+import * as actions from '~/entries/background/common/actions';
+import { storageGetAll } from '~/entries/background/common/storage';
 import { createLogger } from '~/common/log';
-import { why } from '~/common/pretty';
 
 const log = createLogger('Background');
 
-/**
- * @type {{user}}
- */
-let extensionStatus = {};
-
-async function sendMessageToContentScript(type, data = {}) {
-    const tabs = await browser.tabs.query({ active: true });
-
-    const promises = tabs.map(async (tab) => {
-        try {
-            log.debug('SEND <-', `[${tab.id}]`, type, 'sending ...');
-
-            await browser.tabs.sendMessage(tab.id, {
-                type,
-                data,
-            });
-
-            log.debug('SEND <-', `[${tab.id}]`, type, ' ✅ SENT');
-        } catch (err) {
-            log.error('SEND <-', `[${tab.id}]`, type, err);
-        }
-    });
-
-    log.debug('SEND <-', 'sending message', type, 'to', tabs.length, 'tabs:', tabs);
-
-    try {
-        await Promise.all(promises);
-
-        log.debug('SEND <-', '✅ SENT ALL MESSAGES');
-    } catch (err) {
-        log.error('SEND <-', 'error executing promises', err);
-    }
-}
-
-// ------------------------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------------------------------
-
-async function getStatus() {
-    const token = await storageGetToken();
-
-    if (!token) {
-        return { user: null };
-    }
-
-    const { data } = await getExtensionStatus();
-
-    extensionStatus = data?.data || {};
-
-    // WARNING: browser.action is not available in Firefox for some reason
-    if (browser.action) {
-        if (extensionStatus?.accounts?.length > 0) {
-            await browser.action.setBadgeText({ text: `${extensionStatus.accounts.length}` });
-            await browser.action.setBadgeTextColor({ color: '#fff' });
-            await browser.action.setBadgeBackgroundColor({ color: '#f00' });
-        } else {
-            await browser.action.setBadgeText({ text: null });
-        }
-    }
-
-    return {
-        data: data?.data,
-    };
-}
-
-async function logout() {
-    await clearStorage();
-
-    await sendMessageToContentScript(EVENT_REFRESH_AUTH);
-
-    return { success: true };
-}
-
-async function sendPlayerProgress(data) {
-    if (!extensionStatus.user) {
-        log.debug('no status adata, dont send player progress');
-        return;
-    }
-
-    if (extensionStatus.live_streams.length === 0) {
-        log.debug('on stream live');
-        return;
-    }
-
-    await createPlaybackProgress({
-        data,
-        liveStreamId: extensionStatus.live_streams[0].id,
-    });
-}
-
-async function loginFetchUser(accessToken) {
-    try {
-        const { data } = await getAuthenticatedUser({
-            token: accessToken,
-        });
-
-        log.debug('login validate', 'ok');
-        log.debug('storing token...', accessToken);
-
-        await storageSetToken(accessToken);
-
-        return { user: data.data };
-    } catch (err) {
-        log.error('error checking data', err);
-    }
-
-    return { user: null };
-}
-
-async function login() {
-    const redirectUri = browser.identity.getRedirectURL();
-
-    const url = `${import.meta.env.VITE_API_URL}/oauth/authorize?${new URLSearchParams({
-        client_id: import.meta.env.VITE_OAUTH_CLIENT_ID,
-        response_type: 'token',
-        redirect_uri: redirectUri,
-        scope: '',
-    })}`;
-
-    log.debug('login', 'redirect', redirectUri);
-    log.debug('login', url);
-
-    try {
-        // https://cfiiggfhnccbchheekifachmajflgcgn.chromiumapp.org/
-        //     #access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ
-        //     &token_type=Bearer
-        //     &expires_in=31622400
-        const responseUrl = await browser.identity.launchWebAuthFlow({
-            url,
-            interactive: true,
-        });
-
-        const parsedUrl = new URL(responseUrl);
-        const paramsString = parsedUrl.hash.substring(1);
-        const params = new URLSearchParams(paramsString);
-
-        const accessToken = params.get('access_token');
-
-        log.debug('login', 'received success response');
-
-        const response = await loginFetchUser(accessToken);
-
-        await sendMessageToContentScript(EVENT_REFRESH_AUTH, {});
-
-        return response;
-    } catch (err) {
-        log.warn('login', 'error', err);
-
-        return {
-            success: false,
-            error: why(err),
-        };
-    }
-}
-
-async function updateSettingUpdateVisible(data) {
-    await storageSetSettingVisible(data.visible);
-
-    await sendMessageToContentScript(EVENT_REFRESH_SETTINGS);
-
-    return {};
-}
-
-async function openPopup() {
-    // WARNING: browser.action is not available in Firefox for some reason
-    if (!browser.action || !('openPopup' in browser.action)) {
-        log.warn('openPopup', 'not available in browser.action');
-        return {};
-    }
-
-    await browser.action?.openPopup();
-
-    return {};
-}
-
-async function updateScheme({ dark }) {
-    if (!browser.action) {
-        log.warn('updateScheme', 'no browser action available');
-        return;
-    }
-
-    const sizes = ['16', '32', '48', '128'];
-    const icons = {};
-
-    sizes.forEach((size) => {
-        icons[size] = `icons/transparent/${dark ? 'light' : 'dark'}-${size}.png`;
-    });
-
-    log.debug('updateScheme', dark, icons);
-
-    await browser.action.setIcon({
-        path: icons,
-    });
-}
-
-// ------------------------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------------------------------
-
 async function getResponse(type, data) {
-    switch (type) {
-    case OPEN_POPUP:
-        return openPopup();
-    case SET_BROWSER_THEME:
-        return updateScheme(data);
+    const callback = {
+        [messages.OPEN_POPUP]: actions.openPopup,
+        [messages.SET_BROWSER_THEME]: actions.updateScheme,
+        [messages.LOGOUT]: actions.logout,
+        [messages.LOGIN]: actions.login,
+        [messages.GET_STATUS]: actions.getStatus,
+        [messages.PLAYER_PROGRESS]: actions.sendPlayerProgress,
+        [messages.SUGGESTIONS_SEARCH_ACCOUNT]: api.searchSuggestionAccounts,
+        [messages.SUGGESTIONS_SUBMIT]: api.submitSuggestion,
+        [messages.WATCHED_REACTIONS_GET]: api.getWatchedReactions,
+        [messages.REACTION_SUBMIT]: api.submitReaction,
+        [messages.REACTION_POLICY_GET]: api.getReactionPolicy,
+        [messages.CONTENT_RATINGS_GET]: api.getContentRatings,
+        [messages.REACTIONS_GET_FOR_VIDEO]: api.getReactionsForVideo,
+        [messages.REACTIONS_GET_ORIGINAL_VIDEOS]: api.getOriginalVideosForVideo,
+        [messages.SETTING_UPDATE_VISIBLE]: actions.updateSettingUpdateVisible,
+    };
 
-    case LOGOUT:
-        return logout(data);
-    case LOGIN:
-        return login(data);
-    case GET_STATUS:
-        return getStatus(data);
-    case PLAYER_PROGRESS:
-        return sendPlayerProgress(data);
-    case SUGGESTIONS_SEARCH_ACCOUNT:
-        return searchSuggestionAccounts(data);
-    case SUGGESTIONS_SUBMIT:
-        return submitSuggestion(data);
-    case WATCHED_REACTIONS_GET:
-        return getWatchedReactions(data);
-    case REACTION_SUBMIT:
-        return submitReaction(data);
-    case REACTION_POLICY_GET:
-        return getReactionPolicy(data);
-    case CONTENT_RATINGS_GET:
-        return getContentRatings(data);
-    case REACTIONS_GET_FOR_VIDEO:
-        return getReactionsForVideo(data);
-    case REACTIONS_GET_ORIGINAL_VIDEOS:
-        return getOriginalVideosForVideo(data);
-
-    case SETTING_UPDATE_VISIBLE:
-        return updateSettingUpdateVisible(data);
-
-    default:
-        return null;
+    if (callback[type]) {
+        return callback[type](data);
     }
+
+    return null;
 }
 
 export async function handleMessage(msg) {
