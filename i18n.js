e31d8e8b1705ddcf3c76a564f98e46a11068895e
Roman Zipp <ich@ich.wtf>
2024-03-30 14:17:41 +0100

    Refactor loading translations

---
+++ i18n.js
@@ -0,0 +1,9 @@
+import en from '@/locales/en.json';
+import de from '@/locales/de.json';
+
+export const translations = {
+    de,
+    en,
+};
+
+export const defaultLocale = 'en';
--- src/entries/contentScript/main.jsx
+++ src/entries/contentScript/main.jsx
@@ -4,9 +4,8 @@ import { createRoot } from 'react-dom/client';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import en from '@/locales/en.json';
-import de from '@/locales/de.json';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { translations, defaultLocale } from '@/i18n';
 import { renderContent } from './renderContent';
 import App from './App';
 import { createLogger } from '~/common/log';
@@ -15,12 +14,11 @@ i18n
     .use(initReactI18next)
     .use(LanguageDetector)
     .init({
-        resources: {
-            en: { translation: en },
-            de: { translation: de },
-        },
-        lng: 'de',
-        fallbackLng: 'en',
+        resources: Object.entries(translations).reduce((acc, [key, value]) => {
+            acc[key] = { translation: value };
+            return acc;
+        }, {}),
+        fallbackLng: defaultLocale,
         interpolation: {
             escapeValue: false,
         },
