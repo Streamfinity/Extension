185fdeea63ae5ab3186cdf68734cd8f2adf765d6
Roman Zipp <ich@ich.wtf>
2024-03-30 13:31:55 +0100

    Start translations

--- src/entries/contentScript/main.jsx
+++ src/entries/contentScript/main.jsx
@@ -2,9 +2,31 @@ import '~/enableDevHmr';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from '@/locales/en.json';
+import de from '@/locales/de.json';
 import { renderContent } from './renderContent';
 import App from './App';
 import { createLogger } from '~/common/log';
+
+i18n
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        resources: {
+            en: {
+                translation: en,
+            },
+            de: {
+                translation: de,
+            },
+        },
+        lng: 'de',
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false,
+        },
+    });
 
 const log = createLogger('Content-Script');
 const queryClient = new QueryClient();
