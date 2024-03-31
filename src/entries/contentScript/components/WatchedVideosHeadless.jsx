868f2122fbe45df192ecfefede3380a14ad6c30d
Roman Zipp <ich@ich.wtf>
2024-03-31 12:04:42 +0200

    Add youtube watched video info badge
    Resolves #8

--- src/entries/contentScript/components/WatchedVideosHeadless.jsx
+++ src/entries/contentScript/components/WatchedVideosHeadless.jsx
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import moment from 'moment';
+import { useTranslation } from 'react-i18next';
 import { getIdFromLink } from '~/common/utility';
 import { getWatchedReactions } from '~/common/bridge';
 import { createLogger } from '~/common/log';
@@ -7,7 +9,7 @@ import { useAppStore } from '~/entries/contentScript/state';
 
 const log = createLogger('WatchedReactions');
 
-function markElements(watchedVideos) {
+function markElements(watchedVideos, t) {
     let countFound = 0;
     const mediaElements = document.querySelectorAll('ytd-rich-grid-media,ytd-playlist-video-renderer');
 
@@ -27,10 +29,36 @@ function markElements(watchedVideos) {
             return;
         }
 
-        // eslint-disable-next-line no-param-reassign
-        mediaElement.style.transition = 'opacity 200ms';
-        // eslint-disable-next-line no-param-reassign
-        mediaElement.style.opacity = '40%';
+        const fadeOutElements = [
+            mediaElement.querySelector('ytd-thumbnail'),
+            mediaElement.querySelector('#details'),
+        ];
+
+        fadeOutElements.filter((e) => !!e).forEach((element) => {
+            // eslint-disable-next-line no-param-reassign
+            element.style.transition = 'opacity 200ms';
+            // eslint-disable-next-line no-param-reassign
+            element.style.opacity = '40%';
+        });
+
+        const badgeParent = mediaElement.querySelector('#thumbnail');
+
+        if (badgeParent && !mediaElement.querySelector('.streamfinity-watched-badge')) {
+            const infoBadge = document.createElement('div');
+            infoBadge.classList.add('streamfinity-watched-badge');
+            infoBadge.style.padding = '.4rem .6rem';
+            infoBadge.style.backgroundColor = 'red';
+            infoBadge.style.color = 'white';
+            infoBadge.style.fontWeight = '600';
+            infoBadge.style.position = 'absolute';
+            infoBadge.style.top = '0';
+            infoBadge.style.left = '0';
+            infoBadge.style.margin = '1rem';
+            infoBadge.style.borderRadius = '4px';
+            infoBadge.innerText = t('watchedReactions.badgeTitle', { date: moment(watched.reaction_start_date).fromNow() });
+
+            badgeParent.append(infoBadge);
+        }
 
         countFound += 1;
     });
@@ -39,6 +67,7 @@ function markElements(watchedVideos) {
 }
 
 function WatchedVideosHeadless() {
+    const { t } = useTranslation();
     const currentUrl = useAppStore((state) => state.currentUrl);
     const { user } = useAuth();
 
@@ -49,7 +78,7 @@ function WatchedVideosHeadless() {
             }
 
             const watchedVideos = await getWatchedReactions({ userId: user.id });
-            markElements(watchedVideos);
+            markElements(watchedVideos, t);
         })();
     }, [currentUrl, user]);
 
