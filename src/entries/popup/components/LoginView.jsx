195c9d935d4bf2a4bf1e7d3c91dd07f52cd7bbee
Roman Zipp <ich@ich.wtf>
2024-04-27 12:12:03 +0200

    Add popup env

--- src/entries/popup/components/LoginView.jsx
+++ src/entries/popup/components/LoginView.jsx
@@ -5,12 +5,21 @@ import LoginButton from '~/components/LoginButton';
 function LoginView() {
     const { login, loadingLogin } = useAuth();
 
+    const endpoint = new URL(import.meta.env.VITE_API_URL);
+
     return (
         <div className="flex h-full items-center justify-center">
-            <LoginButton
-                loading={loadingLogin}
-                onClick={login}
-            />
+            <div className="w-full">
+                <LoginButton
+                    loading={loadingLogin}
+                    onClick={login}
+                />
+                <div className="mt-2 text-center text-xs text-gray-300">
+                    Environment:
+                    {' '}
+                    {endpoint.hostname}
+                </div>
+            </div>
         </div>
     );
 }
