fddd1fb762e48fe13d4469141f7e4c718374bae8
Roman Zipp <ich@ich.wtf>
2024-05-07 19:36:25 +0200

    Add compact mode

--- src/entries/contentScript/components/StreamerModeNotice.jsx
+++ src/entries/contentScript/components/StreamerModeNotice.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
 import { Button } from '@streamfinity/streamfinity-branding';
 import { useTranslation } from 'react-i18next';
-import Card, { CardTitle } from '~/entries/contentScript/components/Card';
+import Card from '~/entries/contentScript/components/Card';
 import useAuth, { STATE_DEFAULT } from '~/hooks/useAuth';
+import { useAppStore } from '~/entries/contentScript/state';
 
 function StreamerModeNotice() {
     const { t } = useTranslation();
     const { setOverrideState } = useAuth();
 
+    const compact = useAppStore((state) => state.isMinimized);
+
     return (
-        <Card className="flex gap-4">
-            <div className="grow text-sm">
-                <CardTitle>
-                    {t('status.disableStreamerMode')}
-                </CardTitle>
-                <p>
-                    {t('status.disableStreamerModeDescription')}
-                </p>
-            </div>
-            <div className="flex grow items-center">
-                <Button
-                    color="brand-streamer"
-                    className="w-full"
-                    onClick={() => setOverrideState(STATE_DEFAULT)}
-                >
-                    {t('actions.showAll')}
-                </Button>
+        <Card
+            title={t('status.disableStreamerMode')}
+            compact={compact}
+        >
+            <div className="flex gap-2">
+                <div className="grow text-sm">
+                    <p>
+                        {t('status.disableStreamerModeDescription')}
+                    </p>
+                </div>
+                <div className="flex grow items-center">
+                    <Button
+                        color="brand-streamer"
+                        className="w-full"
+                        onClick={() => setOverrideState(STATE_DEFAULT)}
+                    >
+                        {t('actions.showAll')}
+                    </Button>
+                </div>
             </div>
         </Card>
     );
