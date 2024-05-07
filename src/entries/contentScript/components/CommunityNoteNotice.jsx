00021f020fda552a33b2f657fcdbafd348f1d2cc
Roman Zipp <ich@ich.wtf>
2024-05-07 20:55:47 +0200

    Add link to write community notes

--- src/entries/contentScript/components/CommunityNoteNotice.jsx
+++ src/entries/contentScript/components/CommunityNoteNotice.jsx
@@ -17,8 +17,28 @@ function CommunityNoteNotice() {
 
     const { data: notes } = useCommunityNotes({ videoUrl: currentUrl });
 
+    const writeNoteLink = (
+        <a
+            href={buildFrontendUrl(`/dashboard/notes?create_for_video_url=${currentUrl}`)}
+            target="_blank"
+            className="text-sm font-semibold text-primary-700 transition-colors hover:text-primary-900"
+            rel="noreferrer"
+        >
+            {t('communityNotes.add')}
+        </a>
+    );
+
     if (!notes?.length) {
-        return null;
+        return (
+            <Card
+                title={t('communityNotes.title')}
+                color="dashed"
+                compact={compact}
+                preview={t('communityNotes.none')}
+            >
+                {writeNoteLink}
+            </Card>
+        );
     }
 
     const note = notes[0];
@@ -56,7 +76,7 @@ function CommunityNoteNotice() {
                         </a>
                     ))}
                 </p>
-                <div className="mt-2">
+                <div className="mt-2 flex justify-between">
                     <a
                         href={buildFrontendUrl(`/dashboard/notes/review?note_id=${note.id}`)}
                         target="_blank"
@@ -65,6 +85,7 @@ function CommunityNoteNotice() {
                     >
                         {t('communityNotes.rate')}
                     </a>
+                    {writeNoteLink}
                 </div>
                 {(expandable && !isExpanded) && (
                     <div className="absolute bottom-0 left-0 flex h-20 w-full items-end justify-center bg-gradient-to-t from-white from-25% to-white/0">
